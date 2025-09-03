54697de9e43f5ad27dbdaf824f07339a484830f2
ZUMzum02 <paololovon@icloud.com>
2025-09-03 15:10:20 -0500

    Add files via upload

---
+++ both.cpp
@@ -0,0 +1,25 @@
+// Example program
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <cmath>
+using namespace std;
+
+class Both {
+    public:
+    bitset<N> A;
+    bitset<n> M;
+    bitset<n> Q;
+    bool Q1;
+    
+};
+
+int main()
+{
+int num1, num2 ;
+cout << " ingresa el valor para el multiplicando: " << endl;
+cin >> num1;
+cout << " ingresa el valor para el multiplicador; " << endl;
+cin >> num2;
+  return 0;
+}
